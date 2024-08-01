7c6b8d765da9cb4f8aaa9f24a7833d148865c608
DineshKuna <kunadinesh.work@gmail.com>
2024-08-01 07:41:13 +0530

    version5

--- src/frontend/modules/home/travelCheck/TravelCheck.jsx
+++ src/frontend/modules/home/travelCheck/TravelCheck.jsx
@@ -1,27 +1,44 @@
-import React from 'react'
+import React from "react";
 import { CiLocationOn } from "react-icons/ci";
 import { GiPathDistance } from "react-icons/gi";
 import { IoPeopleOutline } from "react-icons/io5";
 import { IoSearchOutline } from "react-icons/io5";
-import './TravelCheck.css'
+import "./TravelCheck.css";
 
 const TravelCheck = () => {
   return (
-    <div  className='travelfeature'>
-      <span>
-      
-        <label htmlFor="location"><CiLocationOn />Location</label>
-        <input type="text" placeholder='Where are you going?'/>
-        <label htmlFor="distance"><GiPathDistance />Distance</label>
-        <input type="text" placeholder='Distance k/m'/>
-        <label htmlFor="maxPeople"><IoPeopleOutline />max People</label>
-        <input type="text" placeholder='0'/>
-        <button><IoSearchOutline /></button>
+    <div className="travelfeature">
+      <span className="travelfeature1">
+        <label className="labelLocation" htmlFor="location">
+          <CiLocationOn />
+          <span className="textlocation">Location</span>
+        </label>
+        <input
+          className="inputLocation"
+          type="text"
+          placeholder="Where are you going?"
+        />
+
+        <label className="labeldistance" htmlFor="distance">
+          <GiPathDistance />
+           <span className="textdistance">Distance</span>
+        </label>
+        <input
+          className="inputdistance"
+          type="text"
+          placeholder="Distance k/m"
+        />
+        <label className="labelmaxpeople" htmlFor="maxPeople">
+          <IoPeopleOutline />
+           <span className="textmaxpeople">Max People</span>
+        </label>
+        <input className="inputmaxpeople" type="text" placeholder="0" />
+        <button className="searchbtn">
+          <IoSearchOutline />
+        </button>
       </span>
-
-
     </div>
-  )
-}
+  );
+};
 
-export default TravelCheck
+export default TravelCheck;
